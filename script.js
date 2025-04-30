69523da12ad7d645af89a67f1b88b1ecb38c0d73
andreoliveira-futurodev <andreoliveirasousa27@outlook.com>
2025-04-29 21:08:03 -0300

    fds-trabalho

---
+++ script.js
@@ -0,0 +1,7 @@
+document.getElementById('msgButton').addEventListener('click', function() {
+    const area = document.getElementById('msgArea');
+    area.textContent = 'Bem-vindo ao meu trabalho de Fundamentos de Desenvolvimento de Software!';
+    area.style.marginTop = '5px';
+    area.style.fontWeight = 'bold';
+  });
+  
